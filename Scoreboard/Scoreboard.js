ef7e3ee5f96a0267b5612ee309e4ba8df645f5bb
unknown <jackdoddpost@gmail.com>
2023-12-02 14:05:26 +1300

    Added scoreboard Project

---
+++ Scoreboard/Scoreboard.js
@@ -0,0 +1,35 @@
+let pCount = 0;
+let pIndex = [];
+let pScore = [];
+function newPlayer(name) {
+  let pID = "player" + pCount;
+  pIndex.push(pID);
+  pScore.push(pCount);
+  const scoreButtons = document.getElementById("template").innerHTML;
+  document.getElementById(pID).innerHTML =
+    "<p>" +
+    name +
+    "</p>" +
+    scoreButtons +
+    "<p id=score" +
+    pCount +
+    ">0</p>" +
+    "</div><div class=scoreBox id=player" +
+    (pCount + 1) +
+    ">";
+    const buttons = document.getElementsByClassName("score-button")
+    for (let button of buttons){
+      button.className = pCount;
+    };
+    pCount += 1;
+}
+function addScore(player, amount) {
+  pScore[player] += amount;
+  document.getElementById("score" + player).innerHTML =
+    "Score: " + pScore[player];
+}
+function removeScore(player, amount) {
+  pScore[player] -= amount;
+  document.getElementById("score" + player).innerHTML =
+    "Score: " + pScore[player];
+}
