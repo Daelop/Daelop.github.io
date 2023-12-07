eb93cb6d351b2fd17d9f127e9a635c7951db607e
unknown <jackdoddpost@gmail.com>
2023-12-07 20:26:30 +1300

    Style update for Scoreboard

--- Scoreboard/Scoreboard.js
+++ Scoreboard/Scoreboard.js
@@ -1,9 +1,12 @@
 let pCount = 0;
-let pIndex = [];
+let pStrikes = [];
 let pScore = [];
+let scoreMax = document.getElementById("maxScore").value;
+let areas = "'header header' 'player0 scoreBar0' ";
+
 function newPlayer(name) {
   let pID = "player" + pCount;
-  pIndex.push(pID);
+  pStrikes.push(0);
   pScore.push(pCount);
   const scoreButtons = document.getElementById("template").innerHTML;
   let newPlayer = document.getElementById(pID);
@@ -14,30 +17,93 @@ function newPlayer(name) {
     scoreButtons +
     "<p id=score" +
     pCount +
-    ">0</p>";
-   
-    let buttons = document.getElementById(pID).getElementsByTagName("button");
-    for (let item of buttons){
-      item.classList.add(pCount);
-      console.log(item.textContent+"class added")
-    }
-  for (let item of buttons){
-      item.classList.remove("score-button");
-      console.log(item.textContent+"class removed")
-   };
-    pCount += 1;
-    let node = document.createElement("div");
-    newPlayer.parentNode.appendChild(node);
-    node.className = "scorebox";
-    node.id = "player"+ pCount;
+    ">Score: 0</p><p class='strikes' id=strikes" +
+    pCount +
+    "></p><div class='scoreBar' id=scoreBar" +
+    pCount +
+    " style='grid-area:scoreBar" +
+    pCount +
+    "; width:10px'></div>";
+  areas += "'player" + (pCount+1) + " scoreBar" + (pCount+1) + "'";
+  let body = document.getElementById("grid");
+  body.style.grid = areas;
+  let buttons = document.getElementById(pID).getElementsByTagName("button");
+  for (let item of buttons) {
+    item.classList.add(pCount);
+    console.log(item.textContent + "class added");
+  }
+  for (let item of buttons) {
+    item.classList.remove("score-button");
+    console.log(item.textContent + "class removed");
+  }
+  pCount += 1;
+  let node = document.createElement("div");
+  newPlayer.parentNode.appendChild(node);
+  node.className = "scoreBox";
+  node.id = "player" + pCount;
+  node.style.gridArea = "player" + pCount;
 }
 function addScore(player, amount) {
   pScore[player] += amount;
   document.getElementById("score" + player).innerHTML =
     "Score: " + pScore[player];
+  // playerBar(player);
 }
-function removeScore(player, amount) {
-  pScore[player] -= amount;
-  document.getElementById("score" + player).innerHTML =
-    "Score: " + pScore[player];
+function playerBar(player){
+  let scoreLength = Math.floor((pScore[player] / scoreMax) * 100) + '%';
+  if (getElementById('scoreBar'+player) = null){
+   let scoreBar = document.createElement("div") 
+   scoreBar.className= 'scoreBar';
+   scoreBar.id= 'scoreBar' + player;
+   scoreBar.style.gridArea= scoreBar + player;
+   scoreBar.style.width = scoreLength
+  }else
+  {
+    let scoreBar = getElementById('scoreBar'+player);
+    scoreBar.style.width = scoreLength
+
+  }
+  
+  
+  
+}
+function strike(player, change) {
+  pStrikes[player] += change;
+  let strikesMax = document.getElementById("maxStrikes").value;
+  let strikeString = "";
+  switch (true) {
+    case pStrikes[player] > strikesMax:
+      {
+        pStrikes[player]--;
+      }
+      break;
+    case pStrikes[player] < 0:
+      {
+        pStrikes[player]++;
+      }
+      break;
+    case pStrikes[player] == strikesMax:
+      {
+        document.getElementById("strikes" + player).innerHTML =
+          "You Are Eliminated!";
+      }
+      break;
+    default: {
+      let strikeCount = pStrikes[player];
+      for (i = 0; i < strikeCount; i++) {
+        strikeString += "X";
+      }
+      document.getElementById("strikes" + player).innerHTML = strikeString;
+    }
+  }
+  console.log(pStrikes[player]);
+}
+function toggleSettings() {
+  if (document.getElementById("settingsWindow").hidden) {
+    document.getElementById("settingsWindow").hidden = false;
+    console.log("unhidden");
+  } else {
+    document.getElementById("settingsWindow").hidden = true;
+    console.log("hidden");
+  }
 }
