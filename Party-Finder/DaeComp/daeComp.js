ba5dac633703dda41af3b2d8945f71ca9a17d878
unknown <jackdoddpost@gmail.com>
2023-12-24 02:24:41 +1300

    DaeComp concept testing

--- Party-Finder/DaeComp/daeComp.js
+++ Party-Finder/DaeComp/daeComp.js
@@ -1,6 +1,44 @@
-function auth(){fetch('`https://discord.com/api/v10/users/@me')
+let token={}
+function auth(){fetch('https://discord.com/api/v10/users/@me')
 .then((response)=>response.json())
 .then((json)=>console.log(json));}
 function test(){
     console.log('fine')
+}
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const code = urlParams.get('code')
+function TokenGen(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+     const code = urlParams.get('code')
+   fetch('https://discord.com/api/oauth2/token',{
+        method: 'POST',
+        headers:{
+            'content-type':'x-www-form-urlencoded'
+        },
+        body:{
+            client_id:'1188079942777438298',
+            client_secret:"4AKGB8vd5qjOiiB1VfsHD7WjglWX3NWL",
+            grant_type:"authorization_code",
+            code: code
+        }
+    })
+    .then ((response)=>response.json())
+    .then ((json)=> token = json)
+}
+function getUser(){
+    fetch('https://discord.com/api/users/@me',{
+        headers:{
+            Authorization:`Bearer ${token}`
+        }
+        
+    }
+ )
+ .then ((response)=>response.json())
+ .then ((json)=>console.log(json))
+}
+function printAuth(){
+    TokenGen();
+    getUser()
 }
