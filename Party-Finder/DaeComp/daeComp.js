217da1af816836f9eafde02deb5f9e9f2eb90b80
unknown <jackdoddpost@gmail.com>
2023-12-24 01:06:20 +1300

    DaeComp concept testing

--- Party-Finder/DaeComp/daeComp.js
+++ Party-Finder/DaeComp/daeComp.js
@@ -1,4 +1,4 @@
-function auth(){fetch('https://discord.com/api/oauth2/authorize?client_id=1188079942777438298&response_type=code&redirect_uri=https%3A%2F%2Fdaelop.github.io%2FParty-Finder%2FDaeComp%2FdaeComp.html&scope=identify')
+function auth(){fetch('/users/@me')
 .then((response)=>response.json())
 .then((json)=>console.log(json));}
 function test(){
