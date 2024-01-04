f7f94868ff38c628492fd3c03b1f5981ee9dc325
unknown <jackdoddpost@gmail.com>
2024-01-05 07:02:34 +1300

    Updated JS for testing

--- Party-Finder/DaeComp/Application.js
+++ Party-Finder/DaeComp/Application.js
@@ -1,25 +1,23 @@
+const fs = require('fs')
 
-class application{
-    constructor(discord, IGN, server, consent, tos){
-        this.discord = discord;
-        this.IGN = IGN;
-        this.server = server;
-        this.consent = consent;
-        this.tos = tos;
-        this.userClass = 'Applicant'
-    }
-}
 
 function submitApp(){
-    const IGN = document.getElementById('pName').value
-    const server = document.getElementById('pServerOCE').value
-    const discord = 'PLACEHOLDER'
-    const consent = document.getElementById('discordConsent').value
-    const tos = document.getElementById('TOS').value
-    fetch ("https://daelop.github.io/Party-Finder/DaeComp/Events/Test_Event/users/users.json",{
-        method: "POST",
-        body: JSON.stringify(new application(discord, IGN, server, consent, tos))
-    }
-    )
+    const application = {
+    'IGN': document.getElementById('pName').value,
+    'server': document.getElementById('pServerOCE').value,
+    'discord': 'PLACEHOLDER',
+    'consent' : document.getElementById('discordConsent').value,
+    'tos' : document.getElementById('TOS').value
+}
+    const filePath= `/Users/Applicants/${application.discord}.JSON`
+    const applicant = JSON.stringify(application)
+    fs.writeFile(filePath,applicant, err => {
+        if (err) {
+            console.log('Error writing file', err)
+        } else {
+            console.log('Successfully wrote file')
+        }
+    });
+
 
 }
